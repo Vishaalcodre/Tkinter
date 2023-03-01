98941381ec7de2d84641d44aaf8b045477f5b928
Vishaalcodre <vishaa369@gmail.com>
2023-03-01 15:14:11 +0530

    Updated Manager

--- Password Manager/pwd_manager.py
+++ Password Manager/pwd_manager.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 import random
@@ -34,17 +35,46 @@ def save():
     web = web_entry.get()
     mail = email_entry.get()
     pwd = pwd_entry.get()
+    new_data = {web: {
+        "email": mail,
+        "password": pwd,
+        }
+    }
 
     if len(web) == 0 or len(mail) == 0 or len(pwd) == 0:
         messagebox.showinfo(message="Please fill every field with details")
     else:
-        is_ok = messagebox.askyesno(title=web, message=f"Do you want to save the following:\nEmail: {mail}\n"f"Password: {pwd}")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{web} | {mail} | {pwd}\n")
-                web_entry.delete(0, END)
-                email_entry.delete(0, END)
-                pwd_entry.delete(0, END)
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
+        finally:
+            web_entry.delete(0, END)
+            email_entry.delete(0, END)
+            pwd_entry.delete(0, END)
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    web = web_entry.get()
+    try:
+        with open("data.json") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="Data File Not Found")
+    else:
+        if web in data:
+            mail = data[web]["email"]
+            pwd = data[web]["password"]
+            messagebox.showinfo(title=web, message=f"Email: {mail}\nPassword: {pwd}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No Details for {web} exists.")
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -61,26 +91,28 @@ canvas.grid(row=0, column=1)
 # Label
 
 website = Label(text="Website")
-Email = Label(text="Email/Username")
-password = Label(text="Password")
 website.grid(row=1, column=0)
+Email = Label(text="Email/Username")
 Email.grid(row=2, column=0)
+password = Label(text="Password")
 password.grid(row=3, column=0)
 
 # Entry
 
-web_entry = Entry(width=35)
+web_entry = Entry(width=22)
 web_entry.focus()
-email_entry = Entry(width=35)
-pwd_entry = Entry(width=25)
-web_entry.grid(row=1, column=1, columnspan=2)
+web_entry.grid(row=1, column=1)
+email_entry = Entry(width=38)
 email_entry.grid(row=2, column=1, columnspan=2)
+pwd_entry = Entry(width=22)
 pwd_entry.grid(row=3, column=1)
 
 # Button
+search_btn = Button(text="Search", width=10, command=find_password)
+search_btn.grid(row=1, column=2)
 pwd_btn = Button(text="Generate Password", command=gen_pwd)
-add_btn = Button(text="Add", width=36, command=save)
 pwd_btn.grid(row=3, column=2, columnspan=2)
+add_btn = Button(text="Add", width=36, command=save)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
